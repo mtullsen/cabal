19f2e5c64d09f4418fb96b12fd8e5802bcee0712
Herbert Valerio Riedel <hvr@gnu.org>
2016-02-26 12:29:55 +0100

    Replace some boilerplate with shorter gmempty/gmappend-boilerplate
    This addresses #3169

--- Cabal/Distribution/Simple/InstallDirs.hs
+++ Cabal/Distribution/Simple/InstallDirs.hs
@@ -113,22 +113,7 @@ instance Functor InstallDirs where
   }
 
 instance (Semigroup dir, Monoid dir) => Monoid (InstallDirs dir) where
-  mempty = InstallDirs {
-      prefix       = mempty,
-      bindir       = mempty,
-      libdir       = mempty,
-      libsubdir    = mempty,
-      dynlibdir    = mempty,
-      libexecdir   = mempty,
-      includedir   = mempty,
-      datadir      = mempty,
-      datasubdir   = mempty,
-      docdir       = mempty,
-      mandir       = mempty,
-      htmldir      = mempty,
-      haddockdir   = mempty,
-      sysconfdir   = mempty
-  }
+  mempty = gmempty
   mappend = (Semi.<>)
 
 instance Semigroup dir => Semigroup (InstallDirs dir) where
